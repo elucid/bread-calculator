e13378b1381d3481c6df8cac41ee8d9b54c9cd2e
Justin Giancola <justin.giancola@gmail.com>
2024-09-21 22:56:08 -0400

    Favicon and title

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,17 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Bread Calculator',
+  title: 'Bread Math',
   description: 'Calculate ingredients for bread making',
+  icons: {
+    icon: [
+      { url: '/icon/favicon.ico' },
+      { url: '/icon/icon.png', type: 'image/png' },
+    ],
+    apple: [
+      { url: '/icon/apple-touch-icon.png' },
+    ],
+  },
 }
 
 export default function RootLayout({
